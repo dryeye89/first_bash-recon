2903625431ad09bf3b9c8deae4883f3cea85191c
dryeye89 <105749335+dryeye89@users.noreply.github.com>
2022-06-01 14:12:46 -0400

    Create basic-recon.sh

---
+++ basic-recon.sh
@@ -0,0 +1,13 @@
+/root/go/bin/subfinder -dL dominios.txt -all -o /root/Automatizacion/archivos/subfinder.txt;
+python3 /root/tools/SubDomainizer/SubDomainizer.py -l dominios.txt -o /root/Automatizacion/archivos/subdomainizer.txt;
+cat /root/Automatizacion/archivos/subfinder.txt /root/Automatizacion/archivos/subdomainizer.txt > /root/Automatizacion/archivos/todo.txt;
+cat /root/Automatizacion/archivos/todo.txt | sort | uniq > /root/Automatizacion/archivos/todo_sindupe.txt;
+cat /root/Automatizacion/archivos/todo_sindupe.txt | /root/go/bin/httprobe > /root/Automatizacion/archivos/activos.txt;
+/root/go/bin/SubOver -l /root/Automatizacion/archivos/activos.txt > /root/Automatizacion/archivos/takeover.txt;
+/root/go/bin/notify -data /root/Automatizacion/archivos/takeover.txt;
+cat /root/Automatizacion/archivos/activos.txt subdominios.txt > /root/Automatizacion/archivos/todos_los_dominios.txt;
+cat /root/Automatizacion/archivos/todos_los_dominios.txt | /root/go/bin/waybackurls | grep "="  | egrep -iv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|icon|pdf|svg|txt|js)" > /root/Automatizacion/archivos/urls_wayback.txt;
+cat /root/Automatizacion/archivos/urls_wayback.txt | /root/go/bin/kxss > /root/Automatizacion/archivos/kxss.txt;
+cat /root/Automatizacion/archivos/kxss.txt | grep ">" > kxss_filtrado.txt;
+echo Notificacion eviada a telegram;
+/root/go/bin/notify -data /root/Automatizacion/archivos/kxss_filtrado.txt;
